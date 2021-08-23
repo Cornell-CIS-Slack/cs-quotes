d8472c4c61d0ad4397673deebea71ac041c64aeb
Edward Tremel <high.order.bits@gmail.com>
2021-08-23 15:22:13 -0400

    Fixed JavaScript loading order
    Standard practice is now to put JavaScript at the bottom of the <body> tag rather than inside the <head>, so that it doesn't load until after the page finishes loading. Also, updated the CSRF cookie setup script to wait until the document is ready to retrieve the cookie.
    
    This should fix a bug I noticed where the upvote buttons sometimes don't work because the ajax-setup script ran before the CSRF cookie was set, and thus AJAX requests don't have a CSRF cookie to send.

--- quotespage/static/quotespage/ajax-setup.js
+++ quotespage/static/quotespage/ajax-setup.js
@@ -1,23 +1,8 @@
-/*Setup code to retrieve Django's anti-CSRF cookie from the browser and add it
+/* Setup code to retrieve Django's anti-CSRF cookie from the browser and add it
  * to the header of all AJAX requests, so that Django will accept them. Copied
- * from the Django documentation at 
- * https://docs.djangoproject.com/en/1.7/ref/contrib/csrf/ 
+ * from the Django documentation at
+ * https://docs.djangoproject.com/en/1.11/ref/csrf/
  */
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
@@ -37,15 +22,18 @@ function sameOrigin(url) {
         !(/^(\/\/|http:|https:).*/.test(url));
 }
 
-var csrftoken = getCookie('csrftoken');
+$(document).ready(function() {
+    var csrftoken = Cookies.get('csrftoken');
 
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-            // Send the token to same-origin, relative URLs only.
-            // Send the token only if the method warrants CSRF protection
-            // Using the CSRFToken value acquired earlier
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                // Send the token to same-origin, relative URLs only.
+                // Send the token only if the method warrants CSRF protection
+                // Using the CSRFToken value acquired earlier
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
         }
-    }
+    });
 });
+
